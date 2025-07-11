ed08c32f0747c7ea50f5412fc7ccd65ca01738a3
Free-Game-Hub <carlos.ehab215@gmail.com>
2025-07-11 18:39:16 +0300

    Add files via upload

---
+++ MC-Schematics-Hub/no-cache-loader.js
@@ -0,0 +1,8 @@
+// no-cache-loader.js
+module.exports = function (req, res, next) {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  next();
+};
